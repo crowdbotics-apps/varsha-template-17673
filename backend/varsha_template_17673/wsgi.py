f34acc03a8a1faf6284cfc2e7ba0ef42f4b0a903
Crowdbotics <team@crowdbotics.com>
2020-06-01 20:58:07 +0000

    Update models

--- backend/varsha_template_17673/wsgi.py
+++ backend/varsha_template_17673/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'varsha_template_17673.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "varsha_template_17673.settings")
 
 application = get_wsgi_application()
